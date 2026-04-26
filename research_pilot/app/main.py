cec97eb069b5ea0ae6bb2575edd602b9c7081bca
Arnav <arnavjain2016@gmail.com>
2026-04-26 13:32:33 -0700

    Fixed CORS for Vercel frontend

--- research_pilot/app/main.py
+++ research_pilot/app/main.py
@@ -17,15 +17,11 @@ UPLOAD_ROOT.mkdir(parents=True, exist_ok=True)
 
 app = FastAPI()
 
-frontend_origin = os.getenv("FRONTEND_ORIGIN", "http://localhost:5173")
+frontend_origin = os.getenv("FRONTEND_ORIGIN", "*")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        frontend_origin,
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
